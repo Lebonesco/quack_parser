9a97eeab16798ae6e12da3479da578131cfd0eed
Joseph <j.d.livni@gmail.com>
2018-10-20 16:53:11 -0700

    operators ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,8 +1,8 @@
 package ast
 
 import (
-	_ "errors"
 	"fmt"
+	"strconv"
 	"github.com/Lebonesco/quack_parser/token"
 )
 
@@ -18,7 +18,10 @@ func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return "LetStatement" }
 
 func (rs *ReturnStatement) statementNode()       {}
-func (rs *ReturnStatement) TokenLiteral() string { return string(rs.Token.Lit) }
+func (rs *ReturnStatement) TokenLiteral() string { return "ReturnStatement" }
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return "ExpressionStatement" }
 
 // Expressions
 func (i *Identifier) expressionNode()      {}
@@ -79,8 +82,36 @@ func NewLetStatement(name, value interface{}) (*LetStatement, error) {
 	return &LetStatement{Name: n, Value: v}, nil
 }
 
+func NewExpressionStatement(expr Attrib) (*ExpressionStatement, error) {
+		return &ExpressionStatement{Expression: expr.(Expression)}, nil
+}
+
+func NewInfixExpression(left, oper, right Attrib) (Expression, error) {
+	l, ok := left.(Expression)
+	if !ok {
+		return nil, fmt.Errorf("invalid left expression. got=%T", left)
+	}
+
+	o, ok := oper.(*token.Token)
+	if !ok {
+		return nil, fmt.Errorf("operator invalid token. got=%T", oper)
+	}
+
+	r, ok := right.(Expression)
+	if !ok {
+		return nil, fmt.Errorf("invalid rigth expression. got=%T", right)
+	}
+
+	return &InfixExpression{Left: l, Operator: string(o.Lit), Right: r}, nil
+}
+
 func NewIntLiteral(integer Attrib) (Expression, error) {
-	return &Identifier{Value: string(integer.(*token.Token).Lit)}, nil
+	intLit, _ := integer.(*token.Token)
+	value, err := strconv.ParseInt(string(intLit.Lit), 0, 64)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse %q as integer", string(intLit.Lit))
+	}
+	return &IntegerLiteral{Token: *intLit, Value: value}, nil
 }
 
 func NewStringLiteral(str Attrib) (Expression, error) {
